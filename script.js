6d9b9651a6a01a66b36bd80e5445b93857816707
Jose Horta <joshhortt@yahoo.com>
2019-04-15 16:03:28 +0100

    separate player 1' from p2'in script.js' to separate their ways of tracking their values.

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ var p2Score = 0;
 
 p1Button.addEventListener("click", function(){
 p1Score++;
-p2Score++;
 p1Display.textContent = p1Score;
+
+});
+
+
+p2Button.addEventListener("click", function(){
+p2Score++;
 p2Display.textContent = p2Score;
+
 });
