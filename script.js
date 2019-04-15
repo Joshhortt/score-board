578583568fdf4d3371f2fdacd2a021c6d0937bda
Jose Horta <joshhortt@yahoo.com>
2019-04-15 15:45:54 +0100

    add span to H1 and variable that gives the span element created in HTML.

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 //  You can usew both options .querySelector or .getElementById
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var p1Display = document.querySelector("#p1Display");
 var p1Score = 0;
 
-p1Button.addEventListener("click", function()
+p1Button.addEventListener("click", function(){
 p1Score++;
-{);
+});
