3bf14b9247898083467d329827c47361f593f3c9
Jose Horta <joshhortt@yahoo.com>
2019-04-15 15:41:43 +0100

    add new variable and eventListener? in script file.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
-//  You can usew both options .querySelector or .getElementById 
+//  You can usew both options .querySelector or .getElementById
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var p1Score = 0;
+
+p1Button.addEventListener("click", function()
+p1Score++;
+{);
