7ce700e9e29bf00aac6cb6bb6b4b018050ce0eff
Jose Horta <joshhortt@yahoo.com>
2019-04-15 15:53:30 +0100

    add span, score, increase score and eventListener to player 2, display 2.

--- script.js
+++ script.js
@@ -3,8 +3,13 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
+var p2Score = 0;
 
 p1Button.addEventListener("click", function(){
 p1Score++;
+p2Score++;
+p1Display.textContent = p1Score;
+p2Display.textContent = p2Score;
 });
